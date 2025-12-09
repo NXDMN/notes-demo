1153869a2760d1d9b1157748331f7baf4e7ba909
NXDMN <norvintsl@gmail.com>
2025-12-09 23:49:47 +0800

    update background color

--- app/create.tsx
+++ app/create.tsx
@@ -56,8 +56,8 @@ const CreateScreen = () => {
       <LinearGradient
         colors={["#1B284F", "#351159", "#421C45", "#3B184E"]}
         locations={[0.1445, 0.4917, 0.7482, 1.0]}
-        start={{ x: 1, y: 1 }}
-        end={{ x: 0, y: 0 }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
         style={{
           flex: 1,
           padding: 20,
